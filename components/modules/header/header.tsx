364f734532b57af2fa95392d9ed984260744ba44
wb-keeper <wb-keeper@mail.ru>
2024-03-04 23:41:53 +0300

    Small changes

--- components/modules/header/header.tsx
+++ components/modules/header/header.tsx
@@ -2,6 +2,7 @@
 import { FC } from "react";
 import { useLang } from "@/hooks/useLang";
 import Logo from "@/components/elements/Logo/Logo";
+import Link from "next/link";
 
 interface HeaderProps {}
 
@@ -10,13 +11,32 @@ const Header: FC<HeaderProps> = () => {
   return (
     <header className="header">
       <div className="container header__container">
-        <button className="btn-reset header__burger">
-          {translations[lang].header.menu_btn}
-        </button>
+        <button className="btn-reset header__burger"></button>
+
         <div className="header__logo">
           <Logo />
         </div>
-        <ul className="header__links"></ul>
+        <ul className="header__links list-reset">
+          <li className="header__links__item">
+            <button className="btn-reset header__links__item__btn header__links__item__btn--search" />
+          </li>
+          <li className="header__links__item">
+            <Link
+              href="/favorites"
+              className="header__links__item__btn header__links__item__btn--favorites"
+            />
+          </li>
+          <li className="header__links__item">
+            <Link
+              className="header__links__item__btn header__links__item__btn--compare"
+              href="/comparison"
+            />
+          </li>
+          <li className="header__links__item"></li>
+          <li className="header__links__item header__links__item--profile">
+            <button className="btn-reset header__links__item__btn header__links__item__btn--profile" />
+          </li>
+        </ul>
       </div>
     </header>
   );

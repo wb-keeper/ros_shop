d8d8354bdfcc234b0e797c8602228703d4d90481
wb-keeper <wb-keeper@mail.ru>
2024-03-09 23:35:57 +0300

    Working on lang switcher

--- components/modules/Header/Menu.tsx
+++ components/modules/Header/Menu.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { useUnit } from "effector-react/effector-react.mjs";
-import { $menuIsOpen } from "@/context/modals";
+import { $menuIsOpen, closeMenu } from "@/context/modals";
 import { useLang } from "@/hooks/useLang";
+import { removeOverflowHiddenFromBody } from "@/lib/utils/common";
+import { setLang } from "@/context/lang";
+import { AllowedLangs } from "@/constants/lang";
 
 export const Menu = () => {
   const [showCatalogList, setShowCatalogList] = useState(false);
@@ -9,9 +12,28 @@ export const Menu = () => {
   const [showContactsList, setShowContactsList] = useState(false);
   const menuIsOpen = useUnit($menuIsOpen);
   const { lang, translations } = useLang();
+  const handleCloseMenu = () => {
+    removeOverflowHiddenFromBody();
+    closeMenu();
+  };
+
+  const handleSwitchLang = (lang: string) => {
+    setLang(lang as AllowedLangs);
+    localStorage.setItem("lang", JSON.stringify(lang));
+  };
+  const handleSwitchLangToRu = () => handleSwitchLang("ru");
+  const handleSwitchLangToEn = () => handleSwitchLang("en");
 
   return (
     <nav className={`nav-menu ${menuIsOpen ? "open" : "close"}`}>
+      <button
+        className={`btn-reset nav-menu__close ${menuIsOpen ? "open" : ""} `}
+        onClick={handleCloseMenu}
+      ></button>
+      <div className={`nav-menu__lang ${menuIsOpen ? "open" : ""}`}>
+        <button>RU</button>
+        <button>EN</button>
+      </div>
       <h1>Menu</h1>
     </nav>
   );

0abdf97890ba86a2607d8641d8dc7716dd9da7b8
wb-keeper <wb-keeper@mail.ru>
2024-04-05 20:21:26 +0300

    Worked on FooterLinks & FooterMobileLinks components

---
+++ components/modules/Footer/FooterLinks.tsx
@@ -0,0 +1,12 @@
+const FooterLinks = () => (
+  <div className="footer__links">
+    <span>
+      <a href="https://vc.ru">vc.ru</a>
+    </span>
+    <span>
+      <a href="https://habr.comu">habr.com</a>
+    </span>
+  </div>
+);
+
+export default FooterLinks;

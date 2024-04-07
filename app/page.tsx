c05a99f25996ab3bb863cc52292042c040ebbdf6
wb-keeper <wb-keeper@mail.ru>
2024-04-07 20:03:28 +0300

    Worked on Footer css

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-// 2 06
+// 2 10
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>page</h1>
+      <h1>page </h1>
     </main>
   );
 }
